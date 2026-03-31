12f02eec4199bae8f1acf81c106c88ae99ae632e
ritik <ritiksharma98010@gmail.com>
2026-04-01 01:22:33 +0530

    Final fix: applied proper gitignore

--- app/model/post.py
+++ app/model/post.py
@@ -7,6 +7,6 @@ class Post(Base):
 
     id=Column(Integer , primary_key=True , index=True)
     title=Column(String , nullable=False)
-    Content=Column(String, nullable=False)
-    Created_at=Column(DateTime, default=datetime.utcnow)
+    content=Column(String, nullable=False)
+    created_at=Column(DateTime, default=datetime.utcnow)
 
