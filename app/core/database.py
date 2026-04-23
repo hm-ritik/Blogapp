534fdc476c97d56a3613a2da8d3ee5902bf1ef92
Ritik Raushan <ritiksharma98010@gmail.com>
2026-04-23 23:53:33 +0530

    "database connection"

--- app/core/database.py
+++ app/core/database.py
@@ -7,14 +7,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 print("DATABASE_URL:", DATABASE_URL)
-
-if DATABASE_URL.startswith("sqlite"):
-    engine = create_engine(
-        DATABASE_URL,
-        connect_args={"check_same_thread": False}
-    )
-else:
-    engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
